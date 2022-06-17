f740afc7bb2bd20cc61da94e638a7fcc4df6e0ea
bakanaouji <abeken1224@yahoo.co.jp>
2022-06-17 11:02:00 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# Execution example
-```sh
-# full infomation feedback
-python main.py --arch mftrl
-# bandit feedback
-python main.py --feedback bandit --arch mftrl --eta 0.01
+# Mutation-Driven Follow the Regularized Leader for Last-Iterate Convergence in Zero-Sum Games
+Code for reproducing results in the paper "Mutation-Driven Follow the Regularized Leader for Last-Iterate Convergence in Zero-Sum Games".
+
+## About
+In this study, we consider a variant of the Follow the Regularized Leader (FTRL) dynamics in two-player zero-sum games.
+FTRL is guaranteed to converge to a Nash equilibrium when time-averaging the strategies, while many variants suffer from the issue of limit cycling behavior, i.e., lack the last-iterate convergence guarantee.
+To resolve this issue, we propose a mutation-driven FTRL (M-FTRL), an algorithm that introduces mutation for the perturbation of action probabilities.
+We then investigate the continuous-time dynamics of M-FTRL and provide the strong convergence guarantees toward stationary points that approximate Nash equilibria under full-information feedback.
+Furthermore, our simulation demonstrates that M-FTRL can enjoy faster convergence rates than FTRL and optimistic FTRL under full-information feedback and surprisingly exhibits clear convergence under bandit feedback.
+
+## Installation
+This code is written in Python 3.
+To install the required dependencies, execute the following command:
+```bash
+$ pip install -r requirements.txt
+```
+
+## Run Experiments
+In order to investigate the performance of M-FTRL in biased Rock-Paper-Scissors under full-information feedback, execute the following command:
+```bash
+$ python run_full_information_feedback_experiment.py
+```
+
+To evaluate M-FTRL via an experiment in biased Rock-Paper-Scissors under bandit feedback, execute the following command:
+```bash
+$ python run_bandit_feedback_experiment.py
 ```
--- run_bandit_feedback_experiment.py
+++ run_bandit_feedback_experiment.py
@@ -15,15 +15,14 @@ MFTRL = 'mftrl'
 FTRLS = [FTRL, MFTRL, OFTRL]
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--feedback', default=ftrl.FULL_FEEDBACK, type=str, choices=(ftrl.FULL_FEEDBACK, ftrl.BANDIT_FEEDBACK))
-parser.add_argument('--payoff', default='payoff/baised_rsp.csv', type=str, help='payoff csv file path using single population')
+parser.add_argument('--payoff', default='payoff/biased_rsp.csv', type=str, help='payoff csv file path using single population')
 parser.add_argument('--n_p', '--n_processes', type=int, default=1, help="number of process different seed")
 parser.add_argument('--n_i', '--n_iterations', type=int, default=1000,
                     help='number of iterations (default: 1000)')
 parser.add_argument('--update_freq', '--uf', type=int, default=0, help='update frequency of reference strategy')
-parser.add_argument('--arch', type=str, default=FTRL,  choices=FTRLS)
+parser.add_argument('--arch', type=str, default=MFTRL,  choices=FTRLS)
 parser.add_argument('--mu', type=float, default=0.01, help='mutation rate')
-parser.add_argument('--eta', type=float, default=0.1, help='learning rate')
+parser.add_argument('--eta', type=float, default=1e-4, help='learning rate')
 parser.add_argument('--outdir', type=str, default='results',
                     help='Directory path to save output files.'
                             ' If it does not exist, it will be created.')
@@ -31,7 +30,6 @@ parser.add_argument('--dir_suffix', type=str, default='Directory suffix to save 
 parser.add_argument('--r_p', '--random_payoff', action='store_true', help='random payoff')
 parser.add_argument('--r_i_p', '--random_init_policy', action='store_true', help='random random policy')
 parser.add_argument('--size', type=int, default=2, choices=(2, 3, 5, 10, 50, 100), help='random payoff size')
-
 parser.add_argument('--seed', type=int, default=0, help="random seed")
 
 
@@ -56,18 +54,18 @@ def run_ftrl(inputs):
         n_actions_tuple = game.n_actions_tuple()
         if args.arch == FTRL:
             agents = [
-                ftrl.FollowTheRegularizedLeader(args.eta, n_actions_tuple[0]), 
+                ftrl.FollowTheRegularizedLeader(args.eta, n_actions_tuple[0]),
                 ftrl.FollowTheRegularizedLeader(args.eta, n_actions_tuple[1])
                 ]
         elif args.arch == OFTRL:
             agents = [
-                ftrl.OptimisticFollowTheRegularizedLeader(args.eta, n_actions_tuple[0]), 
+                ftrl.OptimisticFollowTheRegularizedLeader(args.eta, n_actions_tuple[0]),
                 ftrl.OptimisticFollowTheRegularizedLeader(args.eta, n_actions_tuple[1])
                 ]
         elif args.arch == MFTRL:
             agents = [
-                ftrl.MutageneticFollowTheRegularizedLeader(args.eta, n_actions_tuple[0], args.mu, update_freq = args.update_freq), 
-                ftrl.MutageneticFollowTheRegularizedLeader(args.eta, n_actions_tuple[1], args.mu, update_freq = args.update_freq), 
+                ftrl.MutageneticFollowTheRegularizedLeader(args.eta, n_actions_tuple[0], args.mu, update_freq = args.update_freq),
+                ftrl.MutageneticFollowTheRegularizedLeader(args.eta, n_actions_tuple[1], args.mu, update_freq = args.update_freq),
                 ]
         else:
             assert False
@@ -76,17 +74,18 @@ def run_ftrl(inputs):
 
         ftrl.run_ftrl(
             p_id = process_idx,
-            game=game, 
-            agents = agents, 
+            game=game,
+            agents = agents,
             n_iterations = args.n_i,
             logger = logger_,
-            feedback = args.feedback, 
+            feedback = ftrl.BANDIT_FEEDBACK,
             random_policy = args.r_i_p
         )
         print('Finish seed {}'.format(process_idx))
     except Exception as e:
-        traceback.print_exc()   
+        traceback.print_exc()
         print(e)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -96,12 +95,12 @@ if __name__ == "__main__":
         payoff = args.payoff
         payoff = payoff.replace('.csv', '')
     payoff = payoff.replace('/', '_')
-    args.dir_name = 'arch_{}_{}_feedback_{}_{}'.format(args.arch, payoff, args.feedback, args.dir_suffix)
+    args.dir_name = 'arch_{}_{}_feedback_{}_{}'.format(args.arch, payoff, ftrl.BANDIT_FEEDBACK, args.dir_suffix)
     args.outdir = utils.prepare_output_dir(args, args.outdir)
     utils.set_random_seed(args.seed)
-    
+
     start = time.time()
     main(args)
     elapsed_time = time.time() - start
-    
+
     print(args.outdir, " elapsed_time:{0}".format(elapsed_time) + "[sec]")
